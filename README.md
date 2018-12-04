f97c0936f798cd6bdb7db2469dc922bc42b488ab
Allan Paiste <allan.paiste@vaimo.com>
2018-12-04 19:24:30 +0100

    initial files added (ported over from chromedriver plugin)

---
+++ README.md
@@ -0,0 +1,24 @@
+# binary-geckodriver
+
+Downloads correct geckodriver version for your development platform, whether it's Linux 
+(32bits or 64bits), OSX or Windows.
+
+By default either appropriate version (that matches with installed browser) or latest version of the 
+driver will be downloaded.
+    
+## Configuring Extra
+
+If you want a specific version of GeckoDriver, use:
+
+```json
+{
+  "extra": {
+    "geckodriver": {
+      "version": "0.23.0"
+    }
+  }
+}
+```
+
+If you don't specify GeckoDriver version, either appropriate version (that matches with installed 
+browser) or latest version of the driver will be downloaded.
---
+++ src/Plugin.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Copyright © Laurent Baey. All rights reserved.
+ * See LICENSE.txt for license details.
+ * 
+ * Copyright © Vaimo Group. All rights reserved.
+ * See LICENSE_VAIMO.txt for license details.
+ */
+namespace Vaimo\GeckoDriver;
+
+class Plugin implements \Composer\Plugin\PluginInterface, \Composer\EventDispatcher\EventSubscriberInterface
+{
+    /**
+     * @var \Vaimo\GeckoDriver\Installer
+     */
+    private $driverInstaller;
+    
+    public static function getSubscribedEvents()
+    {
+        return array(
+            \Composer\Script\ScriptEvents::POST_INSTALL_CMD => 'installDriver',
+            \Composer\Script\ScriptEvents::POST_UPDATE_CMD => 'installDriver',
+        );
+    }
+
+    public function activate(\Composer\Composer $composer, \Composer\IO\IOInterface $io)
+    {
+        $this->driverInstaller = new \Vaimo\GeckoDriver\Installer($composer, $io);
+    }
+    
+    public function installDriver()
+    {
+        $this->driverInstaller->installDriver();
+    }
+}
